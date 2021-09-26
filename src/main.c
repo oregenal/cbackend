d6a539b414a0fbed1f83866bc55aa5efedd755bf
Andrey Gud <nosacred@gmail.com>
2021-09-26 21:37:03 +0700

    added string search module

--- src/main.c
+++ src/main.c
@@ -9,13 +9,16 @@
 #include <unistd.h>
 #include <string.h>
 #include "erproc.h"
+#include "str_search_ptrn.h"
 
 char *response = "HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: s3pPLMBiTxaQ9kYGzzhZRbK+xOo=\r\n\r\n";
+char *socket_request = "Sec-WebSocket-Key:";
 
 int main(void)
 {
 	int opt = 1;
 	char buf[BUFFER_SIZE];
+	char request_key[24];
 	struct sockaddr_in addr;
 
 	addr.sin_family = AF_INET;
@@ -38,6 +41,15 @@ int main(void)
 		Write(1, buf, ssize);
 
 		Write(sockfd, response, strlen(response));
+
+		int pos = str_search_ptrn(socket_request, buf, ssize);
+
+		for(int i = 0; i < 24; ++i) {
+			request_key[i] = buf[pos+19+i];
+		};
+
+		printf("position: %d\n", pos);
+		Write(1, request_key, 24);
 
 		/* Close(sockfd); */
 	}
