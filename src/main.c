866b1117e10bf1c2d8942c302c30def0212ef4de
Andrey Gud <nosacred@gmail.com>
2021-09-29 12:47:26 +0700

    regular commit

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #define SERVER_PORT 56765
 #define BUFFER_SIZE 2048
+#define REQUEST_KEY_SIZE 24
 
 #include <stdio.h>
 #include <sys/socket.h>
@@ -20,7 +21,7 @@ int main(void)
 {
 	int opt = 1;
 	char buf[BUFFER_SIZE];
-	char request_key[24];
+	char request_key[REQUEST_KEY_SIZE];
 	char response_key[MESSAGE_SIZE];
 	struct sockaddr_in addr;
 	int response_len = strlen(response);
@@ -41,18 +42,16 @@ int main(void)
 	while(1) {
 		int sockfd = Accept(ls, NULL, NULL);
 
-		ssize_t ssize = Read(sockfd, buf, 2048);
+		ssize_t ssize = Read(sockfd, buf, BUFFER_SIZE);
 		Write(1, buf, ssize);
-
-/*		Write(sockfd, response, strlen(response)); */
 
 		int pos = str_search_ptrn(socket_request, buf, ssize);
 
-		for(int i = 0; i < 24; ++i) {
+		for(int i = 0; i < REQUEST_KEY_SIZE; ++i) {
 			request_key[i] = buf[pos+19+i];
 		};
 
-		Write(1, request_key, 24);
+		Write(1, request_key, REQUEST_KEY_SIZE);
 		printf("\n");
 
 		accept_key_generator(request_key, response_key);
@@ -63,12 +62,13 @@ int main(void)
 		Write(sockfd, response_key, MESSAGE_SIZE);
 		Write(sockfd, "\r\n\r\n", 4);
 
-		sleep(1);
-		
-		Write(sockfd, hello_message, 20);
-
-		/* Close(sockfd); */
-	}
+		while(1) {
+			ssize_t ssize = Read(sockfd, buf, BUFFER_SIZE);
+			printf(" Ssize: %ld.", ssize);
+			Write(1, buf, ssize);
+			printf("\n");
+		};
+	};
 
 	return 0;
 }
